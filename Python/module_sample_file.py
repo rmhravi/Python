4cc16b7944e849e7bf77acd1e1faa1e040f3992d
rmhravi <74697382+rmhravi@users.noreply.github.com>
2024-01-23 10:09:22 +0530

    Add files via upload

---
+++ Python/module_sample_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+msg='module_programming'
+sum_list([1,2,3,4])
+def sum_list(l):
+    sum=0
+    for i in l:
+        sum=sum+i
+    print(sum)
+def printing():
+    print('helloooo')
+if __name__=='__main__':
+    printing()
+
+
+# In[ ]:
+
+
+
+
---
+++ Python/module_sample_file_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+msg='module_programming'
+def sum_list(l):
+    sum=0
+    for i in l:
+        sum=sum+i
+    print(sum)
+def printing():
+    print('helloooo')
+sum_list([1,2,3,4])
+if __name__=='__main__':
+    printing()
+
+
+# In[ ]:
+
+
+
+
